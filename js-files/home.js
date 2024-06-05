955e2f20c37a54c1c71d44c720e141d12496422c
subrata mondal <subratamondal1029@outlook.com>
2024-06-05 13:47:13 +0530

    home page complete

--- js-files/home.js
+++ js-files/home.js
@@ -1,6 +1,6 @@
        // auto typing in heading function
 
-       var typed = new Typed(".auto-text", {
+       const typer = new Typed(".auto-text", {
         strings: [" to API.", "to Learn.", "to Be healthy.", "to get work."],
         typeSpeed: 100,
         backSpeed: 100,
