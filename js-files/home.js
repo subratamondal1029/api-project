e5c48d330bd4748a9cf01381b149a39afb76330c
subrata mondal <subratamondal1029@outlook.com>
2024-06-06 12:21:08 +0530

    adding image slider with swiper.js in work page

--- js-files/home.js
+++
@@ -1,8 +0,0 @@
-       // auto typing in heading function
-       const Typer = new Typed(".auto-text", {
-        strings: [" to API.", "to Learn.", "to Be healthy.", "to get work."],
-        typeSpeed: 100,
-        backSpeed: 100,
-        loop: true
-      });
-    
