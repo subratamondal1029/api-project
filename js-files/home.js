da9046aaa4402c1e5188f824b612b95d4865becd
subrata mondal <subratamondal1029@outlook.com>
2024-06-06 10:19:10 +0530

    adding typed.js in local

--- js-files/home.js
+++ js-files/home.js
@@ -1,6 +1,4 @@
        // auto typing in heading function
-       import Typed from '../node_modules/typed.js/dist/typed.module.js';
-
        const Typer = new Typed(".auto-text", {
         strings: [" to API.", "to Learn.", "to Be healthy.", "to get work."],
         typeSpeed: 100,
